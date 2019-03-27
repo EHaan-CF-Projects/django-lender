2ccfcbc7ae86a69b4d89843b071413ffcf997d10
EvyHaan <evy.haan@gmail.com>
2019-03-27 16:42:56 -0700

    book-user relationship model bug

--- lender_books_app/models.py
+++ lender_books_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Book(models.Model):
     """
     """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(max_length=48)
     author = models.CharField(max_length=48)
     year = models.IntegerField(default=0000)
@@ -26,3 +28,4 @@ class Book(models.Model):
 
     def __str__(self):
         return f'Book: {self.title} | Status: {self.status}'
+
